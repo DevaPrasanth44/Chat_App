cead12c5f3cb1fe7480952b70c1db16f869107e5
Deva <devaprasanth4477@gmail.com>
2026-01-23 12:19:04 +0530

    Files removed

--- client/src/_tests_/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../App';
-
-jest.mock('../Chat', () => () => <div>Chat Component</div>);
-
-test('App renders Chat component', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('Chat Component')).toBeInTheDocument();
-});
