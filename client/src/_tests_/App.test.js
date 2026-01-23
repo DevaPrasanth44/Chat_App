cdfe0ed2856deb24e054aff535876ba79dc9ea3c
Deva <devaprasanth4477@gmail.com>
2026-01-23 12:02:33 +0530

    Changes done

--- client/src/_tests_/App.test.js
+++ client/src/_tests_/App.test.js
@@ -1,7 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render } from '@testing-library/react';
+import App from '../App';
 
-test("renders chat component", () => {
-  render(<App />);
-  expect(screen.getByText(/chat app/i)).toBeInTheDocument();
+jest.mock('../Chat', () => () => <div>Chat Component</div>);
+
+test('App renders Chat component', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Chat Component')).toBeInTheDocument();
 });
